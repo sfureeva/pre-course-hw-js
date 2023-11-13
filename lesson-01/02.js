441e47e4506ca0a3656740b36bbeba797caeef0a
Fureeva Svetlana <svet.i@icloud.com>
2023-11-13 14:22:24 +0200

    test-2-2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,7 @@
 // code
-let number = Math.floor(Math.random() * 100);
-if (number % 2 === 0) {
-    console.log (number, 'четное число');
+let number1 = Math.floor(Math.random() * 100);
+if (number1 % 2 === 0) {
+    console.log (number1, "четное число");
 } else {
-    console.log (number, 'нечетное число');
-}
+    console.log (number1, "нечетное число");
+} 
