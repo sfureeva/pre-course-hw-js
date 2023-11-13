a84d7517c39c2f54b71b2f7bdc4b2d0e07cce275
Fureeva Svetlana <svet.i@icloud.com>
2023-11-13 19:51:00 +0200

    test6-01

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,7 +1,3 @@
 // code
 let str = ['z', 'z', 'z', 'z', 'y', 'z', 'z'];
-if (str.includes('y')) {
-  console.log("нашел");
- } else {
-  console.log("заблудился");
- }
+console.log(str.includes('y') ? "нашел" : "заблудился");
