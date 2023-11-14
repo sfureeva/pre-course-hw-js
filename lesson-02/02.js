62b30e4fecd516bd21171695791260edf0409700
Fureeva Svetlana <svet.i@icloud.com>
2023-11-14 14:03:03 +0200

    test2.2-01

---
+++ lesson-02/02.js
@@ -0,0 +1,11 @@
+// code
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+debugger;
+console.log(factorial(5));
+
