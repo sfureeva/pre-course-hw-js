00c051f9bbc540d874ebd45ea6cbf1dd143f9679
Fureeva Svetlana <svet.i@icloud.com>
2023-11-30 15:57:14 +0200

    nest3.2-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,8 +1,12 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
-
 function createColorString() {
-    return colors.filter(element => element === "черный" ||  element==="красный" ||element=== "желтый").join('-');
+  return colors
+    .filter(
+      (element) =>
+        element === "черный" || element === "красный" || element === "желтый"
+    )
+    .join("-");
 }
 
 console.log(createColorString());
---
+++ lesson-03/02.js
@@ -0,0 +1,11 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+const message = words.indexOf("апельсин"); 
+  if (message >= 0) {
+    console.log("«Ура! нашел»");
+} else  {
+  console.log("«Придется поискать в другом магазине…»");
+}
+
+
+
