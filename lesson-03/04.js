6014c47ad50e33c1f5fdfef50cb2d51ec70f1aa7
Fureeva Svetlana <svet.i@icloud.com>
2023-12-07 16:08:23 +0200

    test3.6-01

--- lesson-03/04.js
+++ lesson-03/04.js
@@ -62,7 +62,7 @@ const users = [
 ];
 /* Выведи в консоль массив с объектами у которых четная id при помощи метода filter */
 
-const users3 = users.filter(function (element) {
+const users33 = users.filter(function (element) {
     // тут приходит элемент массива:
 // {
 //        id: 8,
@@ -75,4 +75,4 @@ const users3 = users.filter(function (element) {
     return element.id % 2 === 0;
   });
 
-console.log(users3)
+console.log(users33)
---
+++ lesson-03/06.js
@@ -0,0 +1,47 @@
+
+const users3 = [
+    {
+        id: 1,
+        user: 'John',
+        surName: 'Doe',
+        address: '123 Main St'
+    },
+    {
+        id: 2,
+        user: 'Tom',
+        surName: 'Johnson',
+        address: '789 Oak St'
+    },
+    {
+        id: 3,
+        user: 'Bob',
+        surName: 'Jones',
+        address: '135 Pine St'
+    },
+    {
+        id: 4,
+        user: 'David',
+        surName: 'Davis',
+        address: '864 Walnut St'
+    },
+    {
+        id: 5,
+        user: 'Mark',
+        surName: 'Taylor',
+        address: '321 Hickory St'
+    },
+];
+
+
+/* for (const i = 0; i < users3.length; i++) {
+    users3[i].isMarried = true;
+};
+  
+  console.log(users3); */
+
+
+  users3.forEach(function(item) {
+    item.isMarried = true
+  });
+  
+  console.log(users3);
